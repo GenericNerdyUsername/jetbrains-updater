70cc6fcfea998e4d91d2f0a47e1fda490c8063f7
GenericNerdyUsername <genericnerdyusername@proton.me>
2022-08-16 16:04:15 +0100

    Make CLion work

--- flake.nix
+++ flake.nix
@@ -3,6 +3,6 @@
   outputs = {...}: rec {
     nixosModules.jetbrains-updater.nixpkgs.overlays = [ overlay ];
     overlay = (final: prev: {jetbrains = (pkgs final) // {jdk = prev.jetbrains.jdk;};});
-    pkgs = source: source.callPackage ./jetbrains/default.nix;
+    pkgs = source: source.callPackage ./jetbrains/default.nix {};
   };
 }
