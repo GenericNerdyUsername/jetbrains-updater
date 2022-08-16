f45a28ae7b7f91ccbd6edb33207fb6bfe22bdd8d
GenericNerdyUsername <genericnerdyusername@proton.me>
2022-08-16 17:04:13 +0100

    Was using the wrong jdk

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Automatic updates for jetbrains products";
   outputs = {...}: rec {
     nixosModules.jetbrains-updater.nixpkgs.overlays = [ overlay ];
-    overlay = (final: prev: {jetbrains = (pkgs final) // {jdk = prev.jetbrains.jdk;};});
-    pkgs = source: source.callPackage ./jetbrains/default.nix {};
+    overlay = (final: prev: {jetbrains = (pkgs final prev.jetbrains.jdk) // {jdk = prev.jetbrains.jdk;};});
+    pkgs = source: jdk: source.callPackage ./jetbrains/default.nix {jdk=jdk;};
   };
 }
