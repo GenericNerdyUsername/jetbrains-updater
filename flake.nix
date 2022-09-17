8dac4a01e44b9f3c4e3a02e51c33e502fcd5922d
GenericNerdyUsername <genericnerdyusername@proton.me>
2022-09-17 13:19:17 +0100

    Add jcef, reshuffle flake.nix, add nixpkgs as an input

--- flake.nix
+++ flake.nix
@@ -1,12 +1,19 @@
+# TODO add plugin support
+# TODO actually use jcef in jdk compile
+
 {
   description = "Automatic updates for jetbrains products";
-  outputs = {...}: rec {
+
+  outputs = { nixpkgs, ... }: rec {
+    overlay = (final: prev: {
+      jcef = final.callPackage ./jcef.nix {};
+      jetbrains = final.callPackage ./jetbrains {}
+        // { jdk = final.callPackage ./jetbrains-jdk.nix {}; };
+    });
     nixosModules.jetbrains-updater.nixpkgs.overlays = [ overlay ];
-    overlay = (final: prev: {jetbrains = (jetbrains final);});
-    jetbrains = pkgs: pkgs.callPackage ./jetbrains {
-      jdk = pkgs.jetbrains.jdk;
-    } // {
-      jdk = pkgs.callPackage ./jetbrains-jdk {};
-    };
+    packages.x86_64-linux = let
+      pkgs = import nixpkgs { system = "x86_64-linux"; overlays = [ overlay ]; config.allowUnfree = true; };
+    in { jetbrains = pkgs.jetbrains; jcef = pkgs.jcef; };
   };
+
 }
